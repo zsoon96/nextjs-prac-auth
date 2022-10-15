fb6e5f96a4dea5d4aefc787685128018e188a6fc
zsoon96 <pres0616@gmail.com>
2022-10-15 23:43:03 +0900

    NextAuth를 이용한 로그인 구현 - NextAuth 파일 및 로그인 화면 생성 (설명 추가)

--- pages/auth/[...nextauth].ts
+++ pages/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import { NextApiRequest } from "next";
+import {NextApiRequest} from "next";
 import NextAuth from "next-auth";
 import Providers from 'next-auth/providers'
 
@@ -6,14 +6,19 @@ import Providers from 'next-auth/providers'
 
 export default NextAuth({
     providers: [
+        // 로그인 인증 방식 설정
         Providers.Credentials({
-           name: "email-password-credentials",
-           credentials: {
-               email: {label:"Email", type: 'email', placeholder: 'test@example.com'},
-               password: {label:"Password", type: 'password'}
-           },
-            async authorize(credentials: Record<any, any>, req:NextApiRequest) {
-               return credentials;
+            // 해당 인증 방식의 이름
+            name: "email-password-credentials",
+            // Next Auth에서 자동으로 Form을 만들어주는데, 해당 Form에 들어갈 내용 입력
+            credentials: {
+                email: {label: "Email", type: 'email', placeholder: 'test@example.com'},
+                password: {label: "Password", type: 'password'}
+            },
+            // Sign In 버튼을 누르면 들어오는 함수
+            // 해당 부분에서 들어온 데이터를 가지고 인증 진행
+            async authorize(credentials: Record<any, any>, req: NextApiRequest) {
+                return credentials;
             }
         })
     ]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,28 @@
-import {signIn, signOut, useSession} from "next-auth/react";
+// @ts-ignore
+import {signIn, signOut, useSession} from "next-auth/client";
 
 // 로그인 화면
 
 export default function Login() {
-  // @ts-ignore
-  const [session, loading] = useSession();
+    // @ts-ignore
+    const [session, loading] = useSession();
 
-  if (session) {
+    // 세션값 여부를 통해 로그인 확인
+
+    // 세션이 있으면, 사용자 정보 & 로그아웃 버튼 보여주기
+    if (session) {
+        return (
+            <>
+                Signed in as {session.user.email} <br/>
+                <button onClick={() => signOut()}>로그아웃</button>
+            </>
+        )
+    }
+    // 세션이 없으면, 안내 문구 & 로그인 버튼 보여주기
     return (
         <>
-          Signed in as {session.user.email} <br/>
-          <button onClick={() => signOut()}>로그아웃</button>
+            Not Signed in <br/>
+            <button onClick={() => signIn()}>로그인</button>
         </>
     )
-  }
-  return (
-      <>
-        Not Signed in <br/>
-        <button onClick={() => signIn()}>로그인</button>
-      </>
-  )
 }
