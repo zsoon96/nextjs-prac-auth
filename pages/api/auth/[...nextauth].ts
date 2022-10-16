eeef50e69719db9280c7b668b7b39466f7560638
zsoon96 <pres0616@gmail.com>
2022-10-16 16:01:16 +0900

    커스텀 로그인 화면에 NextAuth 기능 연결

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,17 +1,19 @@
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
 import {NextApiRequest} from "next";
-import { signIn } from "next-auth/client";
 
 // 로그인 인증을 처리할 파일 생성
 
 export default NextAuth({
     // 로그인 인증 방식 설정하기
     providers: [
-        // 이메일과 패스워드 입력으로 인증하겠다.
+        // 이메일과 패스워드 입력으로 인증
         Providers.Credentials({
+            // 인증 방식 고유 id명
+            id:"email-password-credential",
             // 해당 인증 방식 이름 설정
-            name: "email-password-credential",
+            name: 'Credentials',
+            type: 'credentials',
             // nextAuth에서 자동으로 Form을 만들어주는데, 해당 Form에 들어갈 내용을 입력
             credentials: {
                 email: { label: "Email", type: "email", placeholder: "test@test.com" },
@@ -21,7 +23,15 @@ export default NextAuth({
             // 해당 부분에서 들어온 데이터를 가지고 인증을 진행
             // (지금은 무조건 인증되는 방식으로 처리)
             async authorize(credentials: Record<any, any>, req: NextApiRequest){
-                return credentials;
+
+                const email = credentials.email;
+                const password = credentials.password;
+
+                // 로그인 성공 여부 임시코드
+                if (email === 'test@test.com' && password === 'test'){
+                    return credentials
+                }
+                throw new Error('아이디 혹은 비밀번호가 틀립니다.')
             }
         })
     ],
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,35 @@
-export default function Login() {
-  return (
-    <form>
-      <label>
-        이메일 :
-        <input type="email" name="email" placeholder="test@test.com"/>
-      </label>
+import { signIn } from "next-auth/client";
 
-      <label>
-        비밀번호 :
-        <input type="password" name="password" />
-      </label>
-      <button type="submit">로그인</button>
-    </form>
-  )
+const Login = () => {
+    const login = async (e: any) => {
+        e.preventDefault();
+        // Form 안에서 이메일, 패스워드 가져오기
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        // [...nextauth]에서 설정한 provider 호출
+        const response = await signIn("email-password-credential", {
+            email,
+            password,
+            redirect: false
+        });
+        console.log(response);
+    }
+
+    return (
+        // onSubmit에 login 함수 등록
+        // 로그인 버튼을 클릭하면 login 함수가 실행
+        <form onSubmit={login}>
+            <label>
+                이메일 :
+                <input type="email" name="email" placeholder="test@test.com" />
+            </label>
+            <label>
+                비밀번호 :
+                <input type="password" name="password" />
+            </label>
+            <button type="submit">로그인</button>
+        </form>
+    )
 }
+
+export default Login;
