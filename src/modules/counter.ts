7c732d1531c086fa237e18a879a396734f40c55c
zsoon96 <pres0616@gmail.com>
2022-10-21 08:24:50 +0900

    Redux 활용한 카운터 예제 - 리덕스 모듈 리팩토링(typesafe-actions - 메서드 체이닝 방식2)

--- src/modules/counter.ts
+++ src/modules/counter.ts
@@ -3,15 +3,16 @@ const {createStandardAction} = deprecated
 
 // action type 정의
 // <const>는 타입을 추론 할 수 있도록 하기 위함 > 사용하지 않을 경우 string으로 인식
-export const INCREASE = <const>'counter/INCREASE'
-export const DECREASE = <const>'counter/DECREASE'
-export const INCREASE_BY = <const>'counter/INCREASE_BY'
+// export const INCREASE = <const>'counter/INCREASE'
+// export const DECREASE = <const>'counter/DECREASE'
+// export const INCREASE_BY = <const>'counter/INCREASE_BY'
 
 // action function
 // createStandardAction를 통해 action을 간결하게 생성
-export const increase = createStandardAction(INCREASE)();
-export const decrease = createStandardAction(DECREASE)();
-export const increaseBy = createStandardAction(INCREASE_BY)<number>();
+// 액션함수에 type을 바로 넣어주면 별도로 위와 같이 액션 타입을 정의해주지 않아도 됨
+export const increase = createStandardAction('counter/INCREASE')();
+export const decrease = createStandardAction('counter/DECREASE')();
+export const increaseBy = createStandardAction('counter/INCREASE_BY')<number>();
 
 // 모든 액션 객체들에 대한 타입 정의
 const actions = { increase, decrease, increaseBy }
@@ -26,10 +27,13 @@ type CounterState = {
 const initialState: CounterState = { count: 0 }
 
 // counter reducer
+// createReducer로 리듀서를 쉽게 생성 가능
+// 제네릭으로 리듀서에서 관리할 상태, 리듀서에서 처리할 모든 액션 객체의 타입을 넣어줌
 // 객체 형식으로 입력
 const counter = createReducer<CounterState, CounterAction> (initialState)
-    .handleAction(INCREASE, state => ({ count: state.count + 1 }))
-    .handleAction(DECREASE, state => ({ count: state.count - 1 }))
-    .handleAction(INCREASE_BY, (state, action) => ({ count: state.count + action.payload }))
+    // 액션 타입이 아닌 액션 함수를 넣어도 작동됨
+    .handleAction(increase, state => ({ count: state.count + 1 }))
+    .handleAction(decrease, state => ({ count: state.count - 1 }))
+    .handleAction(increaseBy, (state, action) => ({ count: state.count + action.payload }))
 
 export default counter
