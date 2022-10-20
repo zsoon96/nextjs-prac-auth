1389b061dd56bff518d18bb021781e595244c5b1
zsoon96 <pres0616@gmail.com>
2022-10-21 08:07:35 +0900

    Redux 활용한 카운터 예제 - 리덕스 모듈 리팩토링(typesafe-actions)

--- src/modules/counter.ts
+++ src/modules/counter.ts
@@ -1,19 +1,21 @@
+import {deprecated, ActionType, createReducer} from 'typesafe-actions';
+const {createStandardAction} = deprecated
+
 // action type 정의
 // <const>는 타입을 추론 할 수 있도록 하기 위함 > 사용하지 않을 경우 string으로 인식
 export const INCREASE = <const>'counter/INCREASE'
 export const DECREASE = <const>'counter/DECREASE'
 export const INCREASE_BY = <const>'counter/INCREASE_BY'
 
-// 모든 액션 객체들에 대한 타입 정의
-type CounterAction =
-    | ReturnType<typeof increase>
-    | ReturnType<typeof decrease>
-    | ReturnType<typeof increaseBy>;
-
 // action function
-export const increase = () => ({type: INCREASE})
-export const decrease = () => ({type: DECREASE})
-export const increaseBy = (diff: number) => ({type: INCREASE_BY, payload: diff})
+// createStandardAction를 통해 action을 간결하게 생성
+export const increase = createStandardAction(INCREASE)();
+export const decrease = createStandardAction(DECREASE)();
+export const increaseBy = createStandardAction(INCREASE_BY)<number>();
+
+// 모든 액션 객체들에 대한 타입 정의
+const actions = { increase, decrease, increaseBy }
+type CounterAction = ActionType<typeof actions>
 
 // 리덕스 모듈에서 관리할 상태의 타입 선언
 type CounterState = {
@@ -24,17 +26,11 @@ type CounterState = {
 const initialState: CounterState = { count: 0 }
 
 // counter reducer
-// state와 함수의 반환값이 일치하도록 작성
-function counter(state: CounterState = initialState, action: CounterAction): CounterState {
-    switch (action.type) {
-        case INCREASE:
-            return {count: state.count + 1}
-        case DECREASE:
-            return {count: state.count -1}
-        case INCREASE_BY:
-            return {count: state.count + action.payload}
-        default:
-            return state
-    }
-}
+// 객체 형식으로 입력
+const counter = createReducer<CounterState, CounterAction> (initialState, {
+    [INCREASE]: state => ({ count: state.count + 1 }),
+    [DECREASE]: state => ({ count: state.count - 1 }),
+    [INCREASE_BY]: (state, action) => ({ count: state.count + action.payload})
+})
+
 export default counter
