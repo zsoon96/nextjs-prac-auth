1e86bef917df46bc42fd57f871be6552060a9ee0
zsoon96 <pres0616@gmail.com>
2022-10-26 07:45:48 +0900

    게시판(BoardApp) 예제 - 메인 컴포넌트 리듀서 적용(테스트)

--- pages/board.tsx
+++ pages/board.tsx
@@ -1,9 +1,24 @@
 import BoardInfo from "../src/components/BoardInfo";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../src/modules/store";
+import {useEffect} from "react";
+import {loadBoard} from "../src/modules/board";
+
 
 const Board = () => {
+    const dispatch = useDispatch()
+    const {board} = useSelector((state:RootState) => state.board)
+
+    useEffect(() => {
+        // @ts-ignore
+        dispatch(loadBoard())
+        console.log('board', board)
+    },[])
+
     return (
         <div>
-            <BoardInfo />
+            {/* 불러온 board 데이터 전달 */}
+            <BoardInfo board={board} />
         </div>
     )
 }
