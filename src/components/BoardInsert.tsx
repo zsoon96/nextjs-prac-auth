dadac6c960806b274a8c05632a2ceff726fc731c
zsoon96 <pres0616@gmail.com>
2022-10-29 08:43:47 +0900

    게시판(BoardApp) 예제 - 게시글 작성 컴포넌트에 useForm 적용 (등록/검증/에러메세지)

--- src/components/BoardInsert.tsx
+++ src/components/BoardInsert.tsx
@@ -1,5 +1,10 @@
+
 // 게시글 작성 UI 컴포넌트
+import {useForm} from "react-hook-form";
+
 const BoardInsert = () => {
+    // 등록 & 검증을 위한 register()
+    const { register } = useForm()
 
     return (
         <form>
@@ -12,16 +17,35 @@ const BoardInsert = () => {
             }}>
                 <div style={{color: 'black', marginBottom: '20px'}}>
                     <div style={{ marginBottom: '10px'}}>제목</div>
-                    <input type="title" placeholder="제목을 입력해주세요." style={{padding: '10px', borderRadius: '8px', width:'100%'}}/>
+                    <input
+                        {...register('title', { required: true })}
+                        type="title" placeholder="제목을 입력해주세요."
+                        style={{padding: '10px', borderRadius: '8px', width:'100%'}}
+                    />
                 </div>
+
                 <div style={{color: 'black', marginBottom: '20px'}}>
                     <div style={{ marginBottom: '10px'}}>내용</div>
-                    <input type="content" placeholder="내용을 입력해주세요." style={{padding: '10px', borderRadius: '8px', width:'100%'}}/>
+                    <input
+                        {...register('content', { required: true })}
+                        type="content" placeholder="내용을 입력해주세요."
+                        style={{padding: '10px', borderRadius: '8px', width:'100%'}}
+                    />
                 </div>
+
                 <div style={{color: 'black', marginBottom: '20px'}}>
                     <div style={{ marginBottom: '10px'}}>작성자</div>
-                    <input type="author" placeholder="작성자를 입력해주세요." style={{padding: '10px', borderRadius: '8px', width:'100%'}}/>
+                    <input
+                        {...register('author', {
+                            required: true,
+                            maxLength: { value: 4, message: "4글자 이내로 입력해주세요."},
+                            validate: {noAdmin: (value) => !value.includes('admin') || '해당 입력값은 허용되지 않습니다.'}
+                        })}
+                        type="author" placeholder="작성자를 입력해주세요."
+                        style={{padding: '10px', borderRadius: '8px', width:'100%'}}
+                    />
                 </div>
+                <input type="submit"/>
             </div>
         </form>
     )
