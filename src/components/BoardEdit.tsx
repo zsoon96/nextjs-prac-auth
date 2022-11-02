9ac85fcfe96055ba69592719fa37e7232b883fab
zsoon96 <pres0616@gmail.com>
2022-11-03 07:26:09 +0900

    게시판(BoardApp) 예제 - 게시글 수정 컴포넌트 보완(defaultValue 설정)

--- src/components/BoardEdit.tsx
+++ src/components/BoardEdit.tsx
@@ -13,37 +13,42 @@ interface FormType {
     author: string
 }
 
-const BoardEdit = (id:any) => {
-    // 등록 & 검증을 위한 register()
-    // 입력값 동작을 위한 handleSubmit()
-    // 입력값 초기화를 위한 reset()
-    // 입력값 별도 설정을 위한 setValue()
-    // 손쉬운 에러처리를 위한 formState
-    const { register, handleSubmit, reset, setValue, formState: {errors} } = useForm<FormType>()
+const BoardEdit = (id: any) => {
+    const dispatch = useDispatch()
+    const router = useRouter()
     const [board, setBoard] = useState<FormType>({
         title: '',
         content: '',
         author: ''
     })
 
+    const {register, handleSubmit, reset, setValue, formState: {errors}} = useForm<FormType>()
+
     useEffect(() => {
         axios.get(`http://localhost:3001/board/${id.id}`)
             .then((res) => {
-                console.log('상세 응답', res)
                 setBoard(res.data)
             })
-    },[id])
+    }, [id])
 
     // 입력된 데이터가 유효하다면 실행될 함수
     const onValid = (data: FormType) => {
+        axios.put(`http://localhost:3001/board/${id.id}`, data)
+            .then(async (res) => {
+                console.log('수정 성공', res)
+                // @ts-ignore
+                // dispatch(addBoard(res.data))
+                alert('게시글 수정 성공')
+                await router.replace(`/board/view/${id.id}`)
+            })
     }
     // 입력된 데이터가 유효하지 않다면 실행될 에러 함수
-    const onInvalid = (errors:FieldErrors) => console.log(errors)
+    const onInvalid = (errors: FieldErrors) => console.log(errors)
 
     return (
         // 최종 호출 시, 데이터 처리 함수
         // handleSubmit (유효한 입력 값일 때 실행할 함수, 유효하지 않은 값일 때 실행할 에러 함수)
-        <form onSubmit={handleSubmit(onValid, onInvalid)}>
+        <form noValidate autoComplete="off" onSubmit={handleSubmit(onValid, onInvalid)}>
             <div style={{
                 backgroundColor: 'white',
                 width: '80%',
@@ -52,42 +57,42 @@ const BoardEdit = (id:any) => {
                 padding: '36px 36px',
             }}>
                 <div style={{color: 'black', marginBottom: '20px'}}>
-                    <div style={{ marginBottom: '10px'}}>제목</div>
+                    <div style={{marginBottom: '10px'}}>제목</div>
                     <input
-                        {...register('title', { required: {value: true, message: '필수 입력값입니다.'} })}
+                        {...register('title', {required: {value: true, message: '필수 입력값입니다.'}})}
                         type="title" placeholder="제목을 입력해주세요."
-                        value={board.title}
-                        style={{padding: '10px', borderRadius: '8px', width:'100%'}}
+                        defaultValue={board.title}
+                        style={{padding: '10px', borderRadius: '8px', width: '100%'}}
                     />
-                    <span style={{ color: 'red', fontSize: '14px'}}>{errors.title?.message}</span>
+                    <span style={{color: 'red', fontSize: '14px'}}>{errors.title?.message}</span>
                 </div>
 
                 <div style={{color: 'black', marginBottom: '20px'}}>
-                    <div style={{ marginBottom: '10px'}}>내용</div>
+                    <div style={{marginBottom: '10px'}}>내용</div>
                     <input
-                        {...register('content', { required: {value: true, message: '필수 입력값입니다.'} })}
+                        {...register('content', {required: {value: true, message: '필수 입력값입니다.'}})}
                         type="content" placeholder="내용을 입력해주세요."
-                        value={board.content}
-                        style={{padding: '10px', borderRadius: '8px', width:'100%'}}
+                        defaultValue={board.content}
+                        style={{padding: '10px', borderRadius: '8px', width: '100%'}}
                     />
-                    <span style={{ color: 'red', fontSize: '14px'}}>{errors.content?.message}</span>
+                    <span style={{color: 'red', fontSize: '14px'}}>{errors.content?.message}</span>
                 </div>
 
                 <div style={{color: 'black', marginBottom: '20px'}}>
-                    <div style={{ marginBottom: '10px'}}>작성자</div>
+                    <div style={{marginBottom: '10px'}}>작성자</div>
                     <input
                         {...register('author', {
-                            required: { value: true, message: '필수 입력값입니다.' },
-                            maxLength: { value: 4, message: "4글자 이내로 입력해주세요."},
-                            validate: { noAdmin: (value) => !value.includes('ad') || '해당 입력값은 허용되지 않습니다.' }
+                            required: {value: true, message: '필수 입력값입니다.'},
+                            maxLength: {value: 4, message: "4글자 이내로 입력해주세요."},
+                            validate: {noAdmin: (value) => !value.includes('ad') || '해당 입력값은 허용되지 않습니다.'}
                         })}
                         type="author" placeholder="작성자를 입력해주세요."
-                        value={board.author}
-                        style={{padding: '10px', borderRadius: '8px', width:'100%'}}
+                        defaultValue={board.author}
+                        style={{padding: '10px', borderRadius: '8px', width: '100%'}}
                     />
-                    <span style={{ color: 'red', fontSize: '14px'}}>{errors.author?.message}</span>
+                    <span style={{color: 'red', fontSize: '14px'}}>{errors.author?.message}</span>
                 </div>
-                <input type="submit" style={{padding: '10px', borderRadius: '8px'}} />
+                <input type="submit" style={{padding: '10px', borderRadius: '8px'}}/>
             </div>
         </form>
     )
