20f2ba4f384251d6de04649978f56b79c711e20e
zsoon96 <pres0616@gmail.com>
2022-11-05 10:35:39 +0900

    게시판(BoardApp) 예제 - 게시글 정보 페이지에 수정/삭제 버튼 생성 후, 수정 페이지 연결(Link 적용)

--- src/components/BoardInfo.tsx
+++ src/components/BoardInfo.tsx
@@ -1,4 +1,5 @@
 import Board from "../modules/board";
+import Link from "next/link";
 
 interface Board {
     id: number,
@@ -11,22 +12,27 @@ interface Board {
 type BoardInfoProps = {
     board: Board
 }
-const BoardInfo = ({board} : BoardInfoProps) => {
-    console.log('컴포넌트간 정보 전달', board)
+const BoardInfo = ({board}: BoardInfoProps) => {
+    // console.log('컴포넌트간 정보 전달', board)
+
     return (
-        <div style={{ width: '80%', margin: '36px auto', borderRadius: '10px', padding: '10px 24px'}}>
+        <div style={{width: '80%', margin: '36px auto', borderRadius: '10px', padding: '10px 24px'}}>
             <p>NO {board.id}</p>
-            <hr style={{ border: '0.25px solid gray' }} />
-            <p style={{ fontSize: '36px', fontWeight: 'bold', marginTop:'24px',marginBottom: 0}}>{board.title}</p>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <hr style={{border: '0.25px solid gray'}}/>
+            <p style={{fontSize: '36px', fontWeight: 'bold', marginTop: '24px', marginBottom: 0}}>{board.title}</p>
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
                 <p style={{fontSize: '16px', color: 'lightgray'}}>{board.author}</p>
                 <p style={{fontSize: '16px', color: 'lightgray'}}>{board.regDate}</p>
             </div>
-            <hr style={{ border: '0.25px solid gray' }} />
-            <p style={{ fontSize: '24px', height: '240px'}}>{board.content}</p>
-            <hr style={{ border: '0.25px solid gray' }} />
+            <hr style={{border: '0.25px solid gray'}}/>
+            <p style={{fontSize: '24px', height: '240px'}}>{board.content}</p>
+            <hr style={{border: '0.25px solid gray'}}/>
 
-       </div>
+            <Link href={`/board/edit/${board.id}`}>
+                <button style={{padding: '12px', borderRadius: '8px', border: 'none', marginRight: '8px', marginTop: '4px'}}>수정</button>
+            </Link>
+            <button style={{padding: '12px', borderRadius: '8px', border: 'none'}}>삭제</button>
+        </div>
     )
 }
 
