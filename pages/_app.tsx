2e8e41d843197d3a0e3d1cd7ee0ee426df281f6c
zsoon96 <pres0616@gmail.com>
2022-11-07 06:46:59 +0900

    게시판(BoardApp) 예제 - 서버사이드에서 쿠키로 저장한 엑세스 토큰 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
-import type {AppProps} from 'next/app'
+import type {AppContext, AppProps} from 'next/app'
 import {AuthContextProvider} from "../src/context/AuthContext";
-import rootReducer from "../src/modules";
-import {createStore} from "redux";
 import {Provider} from 'react-redux';
 import store from '../src/modules/store'
+import App from "next/app";
+import cookies from 'next-cookies';
+import {setToken} from "../src/config/TokenManager";
 
 
 // const store = createStore(rootReducer)
@@ -22,6 +23,21 @@ function MyApp({Component, pageProps}: AppProps) {
         </Provider>
         // </Provider>
     )
+}
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+    const appProps:any = await App.getInitialProps(appContext)
+
+    const {ctx} = appContext;
+    // ctx 안에 있는 req에서 쿠키 값 꺼내기
+    // const cookieReq = ctx.req ? ctx.req.headers.cookie : null
+    const cookie = cookies(ctx)
+    const accessToken = cookie['token']
+    if (accessToken !== undefined) {
+        setToken(accessToken)
+    }
+
+    return {...appProps}
 }
 
 export default MyApp
