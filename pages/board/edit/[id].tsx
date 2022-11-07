d199c6bf27d430e159e328c86ce8d032c4d211dd
zsoon96 <pres0616@gmail.com>
2022-11-08 06:14:53 +0900

    게시판(BoardApp) 예제 - 서버사이드에서 쿠키로 저장한 엑세스 토큰 설정 추가 수정

--- pages/board/edit/[id].tsx
+++ pages/board/edit/[id].tsx
@@ -1,9 +1,18 @@
-import {GetStaticPaths, GetStaticProps, GetStaticPropsContext, InferGetStaticPropsType} from "next";
+import {
+    GetServerSideProps,
+    GetServerSidePropsContext,
+    GetStaticPaths,
+    GetStaticProps,
+    GetStaticPropsContext,
+    InferGetStaticPropsType
+} from "next";
 import BoardEdit from "../../../src/components/BoardEdit";
 import axios from "axios";
 import Board from "../index";
+import {AppContext} from "next/app";
+import context from "react-redux/src/components/Context";
 
-interface Board  {
+interface Board {
     id: number
     title: string
     author: string
@@ -12,30 +21,42 @@ interface Board  {
 }
 
 // @ts-ignore
-const BoardEditPage = ({ id } : InferGetStaticPropsType<typeof GetStaticProps>) => {
-    return <BoardEdit id={id} />
+const BoardEditPage = ({id}: InferGetStaticPropsType<typeof GetStaticProps>) => {
+    return <BoardEdit id={id}/>
 }
 
-export const getStaticPaths: GetStaticPaths = async () => {
-    const res = await axios.get('http://localhost:3001/board')
-    const data: Board[] = await res.data
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+    // console.log('ctx', context.params)
 
-    const paths = data.map((board: Board) => ({
-        params: { id: `${board.id}`}
-    }))
+    const params = context.params
 
-    return {
-        paths,
-        fallback: false
-    }
-}
-
-export const getStaticProps: GetStaticProps = ({ params }: GetStaticPropsContext) => {
     return {
         props: {
             id: params?.id
         }
     }
 }
+
+// export const getStaticPaths: GetStaticPaths = async () => {
+//     const res = await axios.get('http://localhost:3001/board')
+//     const data: Board[] = await res.data
+//
+//     const paths = data.map((board: Board) => ({
+//         params: { id: `${board.id}`}
+//     }))
+//
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
+//
+// export const getStaticProps: GetStaticProps = ({ params }: GetStaticPropsContext) => {
+//     return {
+//         props: {
+//             id: params?.id
+//         }
+//     }
+// }
 
 export default BoardEditPage
