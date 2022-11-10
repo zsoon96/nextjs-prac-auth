997264b82d30939a2a54dd42ee6dd0a53bc447e4
zsoon96 <pres0616@gmail.com>
2022-11-11 07:22:33 +0900

    State 동작 방식 예제 - 비동기 함수 > 동기 처리

--- pages/state_prac.tsx
+++ pages/state_prac.tsx
@@ -1,21 +1,33 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 // state 비동기 처리 예제
 const StatePrac = () => {
     const [ count, setCount] = useState(0)
     const [ age, setAge ] = useState(20)
 
-    const handleAgeChange = () => {
-        // 3번째 클릭 시, count = 3
-        setCount(count + 1)
+    // const handleAgeChange = () => {
+    //     // 3번째 클릭 시, count = 3
+    //     setCount(count + 1)
+    //
+    //     // 하지만 비동기 처리이기 때문에 count = 3은 나중에 반영되고 다음 로직 식행
+    //     // 그래서 아직 count는 2인 상태이기 if문을 타게 되는 것
+    //     if ( count < 3 ) {
+    //         setAge(age + 1)
+    //     }
+    // }
+    // 예상 결과 22 > 출력 결과 23
 
-        // 하지만 비동기 처리이기 때문에 count = 3은 나중에 반영되고 다음 로직 식행
-        // 그래서 아직 count는 2인 상태이기 if문을 타게 되는 것
-        if ( count < 3 ) {
+    // 처음 페이지가 로드될 때도 한번 실행이 되기 때문에 초기 값이 20 + 1로 반영
+    useEffect(() => {
+        if (count < 3) {
             setAge(age + 1)
         }
+    },[count])
+
+    const handleAgeChange = () => {
+        setCount(count + 1)
     }
-    // 예상 결과 22 > 출력 결과 23
+    // 예상 결과 22 > 출력 결과 21 - 22 - 23
 
     return (
         <div>
