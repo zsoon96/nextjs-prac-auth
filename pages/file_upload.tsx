e11927a4786116f7fdd3d1e4cbf5eb24e3fbae07
zsoon96 <pres0616@gmail.com>
2022-11-15 06:43:24 +0900

    게시판(BoardApp) 예제 - 다중 파일 업로드

--- pages/file_upload.tsx
+++ pages/file_upload.tsx
@@ -2,23 +2,28 @@ import axios from "axios";
 
 const FileUpload = () => {
 
-    const uploadFile = async(file: File) => {
+    const uploadFile = async(files: any[]) => {
+
         const formData = new FormData()
-        formData.append('files', file)
+
+        files.map((file) => {
+            formData.append('files', file)
+        })
+
         formData.append('type', 'file')
 
         const res = await axios.post('http://localhost:3001/file/upload', formData )
         console.log(res)
     }
 
-    const handleChangeFile = (e:any) => {
-        const [file] = [...e.target.files]
-        uploadFile(file)
+    const handleChangeFile = ({target} :any) => {
+        const files = Array.from(target.files)
+        uploadFile(files)
     }
 
     return (
         <div>
-            <input id='file' type='file' onChange={handleChangeFile} />
+            <input id='file' type='file' multiple={true} onChange={handleChangeFile} />
             <label htmlFor='file'> 파일 탐색</label>
         </div>
     )
