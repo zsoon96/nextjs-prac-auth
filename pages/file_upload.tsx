c1af1cc8cd6a3220e014b0eed13dd534f7d54e21
zsoon96 <pres0616@gmail.com>
2022-11-15 07:26:33 +0900

    게시판(BoardApp) 예제 - 파일 사이즈 변환

--- pages/file_upload.tsx
+++ pages/file_upload.tsx
@@ -6,7 +6,7 @@ const FileUpload = () => {
     const [files, setFiles] = useState<any[]>([]);
 
     const uploadFile = async(files: any[]) => {
-
+        console.log(files)
         const formData = new FormData()
 
         files.map((file) => {
@@ -25,6 +25,23 @@ const FileUpload = () => {
         setFiles(files)
     }
 
+    // 사이즈 단위 변환 (byte > kb/mb/gb)
+    const getByteSize = (size :number) => {
+        // 각 데이터의 크기는 1024의 제곱
+        // 1,024byte == 1kb / 1,024kb == 1mb / 1,024mb == 1gb
+        const byteUnits = ['KB','MB','GB']
+
+        for (let i = 0; i < byteUnits.length; i++) {
+            // math.floor: 소수점 이하 버림
+            size = Math.floor(size / 1024)
+
+            if (size < 1024) {
+                // toFixed: 소수점 이하 자리수 설정
+                return size.toFixed(1) + byteUnits[i]
+            }
+        }
+    }
+
     return (
         <div>
             <input id='file' type='file' multiple={true} onChange={handleChangeFile} />
@@ -33,7 +50,7 @@ const FileUpload = () => {
             <hr style={{ 'marginTop' : '14px', 'marginBottom': '14px'}}/>
 
             {files.length !== 0 ? files.map((file) => <div style={{ marginTop: '10px'}}>
-                {file.name} - {file.size}
+                {file.name} - {getByteSize(file.size)}
             </div>) : null}
         </div>
     )
