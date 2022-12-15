88d4473315671800d226b035f64bcedc21fcb9c3
zsoon96 <pres0616@gmail.com>
2022-12-15 23:23:21 +0900

    게시판(BoardApp) 예제 - 동적라우팅 데이터 페칭 방법 정리(CSR/SSR/SSG)

--- pages/board/view/[id].tsx
+++ pages/board/view/[id].tsx
@@ -10,9 +10,19 @@ interface Board {
     regDate: string
 }
 
+// 동적라우팅 데이터 페칭 방법 정리
+// 1. CSR > router.query로 id값 추출 후 props로 넘겨서 csr로 페칭하는 방법
+// 2. SSR > router.query로 id값 추출 후 getServerSideProps에서 ssr로 페칭까지 하고 id와 data를 props로 넘기는 방법
+// 3. SSG > getStaticPaths를 통해 동적라우팅할 paths 범위 지정 후 params로 전달, getStaticProps를 통해 id값을 추출 후, props로 넘겨서 crs로 페칭하는 방법
+
+// SSG vs SSR
+// SSG : 정적 생성된 정보를 각 요청에 동일한 정보로 반환하는 경우에 사용 / 빌드 시 사전에 API 요청을 통해 데이터를 담고, 데이터가 담긴 HTML을 생성
+// SSR : 사용자의 요청마다 동적으로 페이지를 생성해 다른 내용을 보여주어야 하는 경우에 사용
+
 // board는 getServerSideProps에서 조회해서 받은 정보
 const BoardDetail = ({board}: any) => {
     const router = useRouter()
+    // 요청 url에서 받아온 id 값 추출 후, props로 전달
     const {no}: any = router.query
 
     // url의 id값으로 비동기 통신 요청 시도 1 - useEffect로 데이터를 가져오면 network error 발생
